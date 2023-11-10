c9cc7d0505be3131380c418ea122b74ee0a57c32
nijyaaa <ndf4700@students.uc-bcf.edu.ph>
2023-11-10 15:13:58 +0800

    added coffee shops

--- TriForce-ELT/app/src/main/java/com/example/elt2/ELTDashboard.kt
+++ TriForce-ELT/app/src/main/java/com/example/elt2/ELTDashboard.kt
@@ -1,11 +1,19 @@
 package com.example.elt2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class ELTDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_eltdashboard)
+
+        val coffeeButton = findViewById<ImageButton>(R.id.imageButton1)
+        coffeeButton.setOnClickListener{
+            val Intent = Intent(this, CoffeeSpots::class.java)
+            startActivity(Intent)
+        }
     }
 }
